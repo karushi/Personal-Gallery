7dfabc9539412c5357b7033cbbf1ccf360514d25
karushi <you@example.com>
2018-02-23 12:55:21 +0300

    add changes

---
+++ gallery/photos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to Photo Gallery')
+
+def welcome(request):
+    return render(request, 'welcome.html')    
+
