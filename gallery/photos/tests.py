fc2830e170c2e6a7c6f92bd96954f2ea1c7b75da
karushi <you@example.com>
2018-02-24 17:41:37 +0300

    create test

--- gallery/photos/tests.py
+++ gallery/photos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Location,Gallery
 
 # Create your tests here.
+
+class GalleryTestClass(TestCase):
+    def setUp(self):
+        self.photos=Gallery(image='image',image_name='name',description='describe')
+#Testign instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photos,Gallery))
+
+
+
+
+
+
