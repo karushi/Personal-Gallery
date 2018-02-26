e972a8941aff7c0a5efb55312beae969d123b3e4
karushi <you@example.com>
2018-02-26 18:10:55 +0300

    create a category route which will be the id of the category

--- gallery/photos/urls.py
+++ gallery/photos/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
+    url(r'^image/(\d+)', views.image, name='image'),
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
